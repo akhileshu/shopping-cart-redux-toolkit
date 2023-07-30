62374e780a931d1ceb828820d2193ddc66327aaf
Akhilesh Kumar Upadhyay <umapati4381@gmail.com>
2023-07-30 12:36:43 +0530

    fixed errors

--- src/features copy/cart/cartAPI.js
+++ src/features copy/cart/cartAPI.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const API_BASE_URL = "";
+// Set the relative URL for your JSON Server API
+const API_BASE_URL = "/api"; // Change "/api" to the appropriate base URL of your JSON Server
 
 export async function fetchItems() {
   try {
-    const response = await axios.get(API_BASE_URL);
+    const response = await axios.get(`${API_BASE_URL}/items`);
     return response.data;
   } catch (error) {
-    // Handle errors appropriately
     console.error("Error fetching items:", error);
     throw error;
   }
@@ -15,10 +15,9 @@ export async function fetchItems() {
 
 export async function addItem(item) {
   try {
-    const response = await axios.post(API_BASE_URL, item);
+    const response = await axios.post(`${API_BASE_URL}/items`, item);
     return response.data;
   } catch (error) {
-    // Handle errors appropriately
     console.error("Error adding item:", error);
     throw error;
   }
@@ -26,10 +25,9 @@ export async function addItem(item) {
 
 export async function deleteItem(id) {
   try {
-    const response = await axios.delete(`${API_BASE_URL}/${id}`);
+    const response = await axios.delete(`${API_BASE_URL}/items/${id}`);
     return response.data;
   } catch (error) {
-    // Handle errors appropriately
     console.error("Error deleting item:", error);
     throw error;
   }
@@ -37,12 +35,9 @@ export async function deleteItem(id) {
 
 export async function updateItem(id, updatedItem) {
   try {
-    // console to debug
-    // console.log(id, updatedItem)
-    const response = await axios.patch(`${API_BASE_URL}/${id}`, updatedItem);
+    const response = await axios.patch(`${API_BASE_URL}/items/${id}`, updatedItem);
     return response.data;
   } catch (error) {
-    // Handle errors appropriately
     console.error("Error updating item:", error);
     throw error;
   }
--- src/features/products/productsAPI.js
+++ src/features/products/productsAPI.js
@@ -1,15 +1,14 @@
+
 import axios from "axios";
 
-const API_BASE_URL = "";
+const API_BASE_URL = "/api"; // Use "/api" as the base URL for your JSON Server
 
 export async function fetchProducts() {
   try {
-    const response = await axios.get(API_BASE_URL);
+    const response = await axios.get(`${API_BASE_URL}/products`);
     return response.data;
   } catch (error) {
-    // Handle errors appropriately
     console.error("Error fetching products:", error);
     throw error;
   }
 }
-
